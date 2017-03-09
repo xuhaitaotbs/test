a437c6dbdbaeaf3adc41cc6d33ec9d9668983b5c
xuhaitaots <xuhaitao_jp@163.com>
2017-03-09 23:23:44 +0800

    xuhaitao

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { ProfileState } from "./profile.state";
 import { ProfileStore } from "./profile.store";
 import { NavController } from "ionic-angular/index";
 import { TabsPage} from '../tabs/tabs';
+import { Camera} from 'ionic-native';
 
 @Component({
   selector: "profile",
@@ -12,6 +13,8 @@ import { TabsPage} from '../tabs/tabs';
 })
 
 export class ProfileComponent implements OnInit {
+
+  public base64Image: string;
 
   constructor(private action: ProfileAction,
          private state: ProfileState,
@@ -27,5 +30,18 @@ export class ProfileComponent implements OnInit {
   public onSave() {
       // 設定画面へ遷移する
       this.navCtrl.push(TabsPage, {"user":"haitao.a.xu"});
+  }
+
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
   }
 }

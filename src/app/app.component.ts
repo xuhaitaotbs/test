99d18e56de734aab6dde43b2710c5ccd293c9969
haitao.a.xu <haitao.a.xu@accenture.com>
2017-03-16 18:47:26 +0800

    xuhaitao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Platform, AlertController } from 'ionic-angular';
+import { StatusBar, Push, Splashscreen } from 'ionic-native';
 import { LoginComponent } from '../pages/login/login';
 import { ProfileComponent } from "../pages/profile/profile";
 import { PasswordsetComponent } from "../pages/passwordset/passwordset";
+import { AboutPage } from "../pages/about/about";
 
 @Component({
   templateUrl: 'app.html'
@@ -14,12 +15,67 @@ export class MyApp {
 
   rootPage = LoginComponent;
 
-  constructor(platform: Platform) {
+  constructor(public platform: Platform, public alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.initPushNotification();
+    });
+  }
+
+  initPushNotification(){
+    if (!this.platform.is('cordova')) {
+      console.warn("Push notifications not initialized. Cordova is not available - Run in physical device");
+      return;
+    }
+    let push = Push.init({
+      android: {
+        senderID: "8c384c845a6e3326e8f4b464"
+      },
+      ios: {
+        alert: "true",
+        badge: false,
+        sound: "true"
+      },
+      windows: {}
+    });
+
+    push.on('registration', (data) => {
+      console.log("device token ->", data.registrationId);
+      //TODO - send device token to server
+    });
+    push.on('notification', (data) => {
+      console.log('message', data.message);
+      let self = this;
+      //if user using app and push notification comes
+      if (data.additionalData.foreground) {
+        // if application open, show popup
+        let confirmAlert = this.alertCtrl.create({
+          title: 'New Notification',
+          message: data.message,
+          buttons: [{
+            text: 'Ignore',
+            role: 'cancel'
+          }, {
+            text: 'View',
+            handler: () => {
+              //TODO: Your logic here
+              self.nav.push(AboutPage, {message: data.message});
+            }
+          }]
+        });
+        confirmAlert.present();
+      } else {
+        //if user NOT using app and push notification comes
+        //TODO: Your logic on click of push notification directly
+        self.nav.push(AboutPage, {message: data.message});
+        console.log("Push notification clicked");
+      }
+    });
+    push.on('error', (e) => {
+      console.log(e.message);
     });
   }
 
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,67 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavParams } from "ionic-angular";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-   
-   public websocket : any;
-
-   constructor(public navCtrl: NavController) {
-      //判断当前浏览器是否支持WebSocket
-     if('WebSocket' in window){
-        this.websocket = new WebSocket("ws://localhost:9090/boot");
-      } 
-      else{
-        alert('Not support websocket')
+    pushMessage: string = "push message will be displayed here";
+    constructor(public params: NavParams) {
+      if (params.data.message) {
+        this.pushMessage = params.data.message;
       }
-
-    // //连接发生错误的回调方法
-    // this.websocket.onerror = function(){
-    //     setMessageInnerHTML("error");
-    // };
-
-    // //连接成功建立的回调方法
-    // this.websocket.onopen = function(event){
-    //     setMessageInnerHTML("open");
-    // }
-
-    // //接收到消息的回调方法
-    // this.websocket.onmessage = function(event){
-    //     alert('alert')
-    //     setMessageInnerHTML(event.data);
-    // }
-
-    // //连接关闭的回调方法
-    // this.websocket.onclose = function(){
-    //     setMessageInnerHTML("close");
-    // }
-
-    // //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-    // // window.onbeforeunload = function(){
-    // //     this.websocket.close();
-    // // }
-
-    // //将消息显示在网页上
-    // function setMessageInnerHTML(innerHTML){
-    //     document.getElementById('message').innerHTML += innerHTML + '<br/>';
-    // }
-
-    // //关闭连接
-    // function closeWebSocket(){
-    //     this.websocket.close();
-    // }
-
-    // //发送消息
-    // function send(){
-    //     this.websocket.send('admin');
-    // }
-
-   }
-
-
-    
+    }
 }

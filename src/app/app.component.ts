f884e4e9c3d20a64d344beb5b2162ceb57a979b3
haitao.a.xu <haitao.a.xu@accenture.com>
2017-04-14 17:14:57 +0800

    xuhaitao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,66 +23,11 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-      this.initPushNotification();
     });
 
-    Backend.establish(() => {
-	console.info("Server connected");
-    })
-  }
-
-  initPushNotification(){
-    if (!this.platform.is('cordova')) {
-      console.warn("Push notifications not initialized. Cordova is not available - Run in physical device");
-      return;
-    }
-    let push = Push.init({
-      android: {
-        senderID: "8c384c845a6e3326e8f4b464"
-      },
-      ios: {
-        alert: "true",
-        badge: false,
-        sound: "true"
-      },
-      windows: {}
-    });
-
-    push.on('registration', (data) => {
-      console.log("device token ->", data.registrationId);
-      //TODO - send device token to server
-    });
-    push.on('notification', (data) => {
-      console.log('message', data.message);
-      let self = this;
-      //if user using app and push notification comes
-      if (data.additionalData.foreground) {
-        // if application open, show popup
-        let confirmAlert = this.alertCtrl.create({
-          title: 'New Notification',
-          message: data.message,
-          buttons: [{
-            text: 'Ignore',
-            role: 'cancel'
-          }, {
-            text: 'View',
-            handler: () => {
-              //TODO: Your logic here
-              self.nav.push(AboutPage, {message: data.message});
-            }
-          }]
-        });
-        confirmAlert.present();
-      } else {
-        //if user NOT using app and push notification comes
-        //TODO: Your logic on click of push notification directly
-        self.nav.push(AboutPage, {message: data.message});
-        console.log("Push notification clicked");
-      }
-    });
-    push.on('error', (e) => {
-      console.log(e.message);
-    });
+    // Backend.establish(() => {
+    //  	console.info("Server connected");
+    // })
   }
 
   public personList() {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,11 @@
+import 'rxjs';
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { Backend } from '../../../providers/backend';
+
+import { Platform } from 'ionic-angular';
+import { JPushService } from 'ionic2-jpush';
 
 @Component({
   selector: 'page-home',
@@ -9,12 +13,60 @@ import { Backend } from '../../../providers/backend';
 })
 export class HomePage {
   xxx: string;
-  constructor(public navCtrl: NavController) {
-      Backend.socket.on("MESSAGE_CREATE", (response) => {
-			    //this.logs.push(JSON.stringify(response.message));
-					this.xxx = JSON.stringify(response.message);
-		})
+  constructor(public navCtrl: NavController,
+	    private platform: Platform,
+			private jPushPlugin: JPushService) {
+
+				platform.ready().then( () =>{
+
+             this.jPushPlugin.openNotification()
+               .subscribe( res => {
+                 console.log('收到推送');
+                 console.log(res)
+               });
+       
+             this.jPushPlugin.receiveNotification()
+               .subscribe( res => {
+                 console.log('收到推送');
+                 console.log(res)
+               });
+       
+             this.jPushPlugin.receiveMessage()
+               .subscribe( res => {
+                 console.log('收到推送');
+                 console.log(res)
+               });
+       
+           })
+
+
+
+
+    //   Backend.socket.on("MESSAGE_CREATE", (response) => {
+		// 	    //this.logs.push(JSON.stringify(response.message));
+		// 			this.xxx = JSON.stringify(response.message);
+		// })
   }
+
+	 /**
+    * 注册极光
+    */
+   init() {
+    this.jPushPlugin.init()
+    .then(res => alert(res))
+    .catch(err => alert(err))
+    }
+
+		 /**
+    * 获取ID
+    */
+    getRegistrationID() {
+     this.jPushPlugin.getRegistrationID()
+     .then(res => alert(res))
+     .catch(err => alert(err))
+     }
+
+
 
 	send() {
 		Backend.socket.emit("MESSAGE_CREATE", {
